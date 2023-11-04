98c5fa9f70b68e4b1cc09b4fa0fedc80a436dfbc
komalkumari <keshrikomal887@gmail.com>
2023-11-04 07:56:10 -0500

    added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import SignUp from './Components/SignupPage/Signup';
-import Navbar from './Components/Navbar/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import SignUp from "./Components/SignupPage/Signup";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-     <SignUp/>
+      <header>
+        <Navbar />
+      </header>
+
+      <SignUp />
     </div>
   );
 }
